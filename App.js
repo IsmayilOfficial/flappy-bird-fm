4b1fa3b0fe085ccdd2ee2544e5790c02a55f3d1a
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-13 16:30:49 +0400

    init

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import Bird from './components/Bird'
 export default function App() {
+
+  const screenWidth = Dimensions.get("screen").width
+  const screenHeight = Dimensions.get("screen").height
+
+  console.log(screenWidth)
+  console.log(screenHeight)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hi isi</Text>
+      <Bird />
     </View>
   );
 }
