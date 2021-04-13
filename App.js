45be8d3dd5d2a0dc03a579ab556b362d5cbf590b
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-13 17:46:23 +0400

    obstacles

--- App.js
+++ App.js
@@ -1,19 +1,78 @@
-import React from 'react';
+import { registerRootComponent } from 'expo';
+import React, { useState , useEffect} from 'react'
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import Bird from './components/Bird'
+import Obstacles from './components/Obstacles';
 export default function App() {
 
   const screenWidth = Dimensions.get("screen").width
   const screenHeight = Dimensions.get("screen").height
+  const birdLeft = screenWidth / 2
+  const [birdBottom, setBirdBottom ] = useState(screenHeight/2)
+  const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth)
+  const obstacleWidth = 60
+  const obstacleHeight = 300 
+  const gap = 300
+  const gravity = 3 
+  let gameTimerId
+  let obstaclesLeftTimerId
 
-  console.log(screenWidth)
-  console.log(screenHeight)
+  // start bird falling
+  useEffect(() => {
+     if (birdBottom>0) {
+      gameTimerId =  setInterval(() => {
+        setBirdBottom(birdBottom => birdBottom - gravity)
+       },30)
+
+       return () => {
+          clearInterval(gameTimerId)
+      }
+     }
+   
+    
+  },[birdBottom])
+  console.log(birdBottom)
+
+  //start first obstacles
+
+  useEffect (() => {
+    if (obstaclesLeft > -obstacleWidth) {
+
+     obstaclesLeftTimerId = setInterval(() =>{
+          setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5 )
+      },30)
+      
+      return () => {
+        clearInterval(obstaclesLeftTimerId)
+      }
+  
+    }else{
+      setObstaclesLeft(screenWidth)
+    }
+  },[obstaclesLeft])
+
+
 
   return (
     <View style={styles.container}>
-      <Text>Hi isi</Text>
-      <Bird />
+      
+      <Bird 
+      
+      birdBottom = {birdBottom}
+      birdLeft = {birdLeft}
+
+      />
+
+      <Obstacles
+      obstacleWidth = {obstacleWidth}
+      obstacleHeight = {obstacleHeight}
+      gap = {gap}
+      obstaclesLeft = {obstaclesLeft}
+
+      />
     </View>
+
+   
   );
 }
 
@@ -24,4 +83,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
--- components/Bird.js
+++ components/Bird.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import {  View } from 'react-native';
+import React from 'react'
+import {  View } from 'react-native'
 
-const Bird = () =>{
+const Bird = ({birdBottom, birdLeft}) =>{
+
+    const birdWidth = 50
+    const birdHeigth = 60
 
     return(
         <View style={{
 
             position: 'absolute',
             backgroundColor: 'blue',
-            width: 50 ,
-            height: 60,
+            width: birdWidth ,
+            height: birdHeigth,
+            left: birdLeft - (birdWidth/2),
+            bottom: birdBottom - (birdHeigth/2),
+
 
         }} / >
 
